6fe5288d8b322b281ac53af3c8c8e73233ac0606
Pawan <ps7400356@gmail.com>
2024-07-03 09:28:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Hero from './Commponent/Hero/Hero'
+import Navbar from './Commponent/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+     <div>
+      <Navbar/>
+      <Hero/>
+     </div>
+    </>
+  )
+}
+
+export default App
